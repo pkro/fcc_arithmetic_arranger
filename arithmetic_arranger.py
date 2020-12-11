b5e01885b5cc1e11d2b5d30e30dae9ec9b5bc0dd
pkro <pkrombach@web.de>
2020-12-11 16:55:16 +0100

    passes all error handling tests

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -1,12 +1,34 @@
-def arithmetic_arranger(problems, solve=False):
-    arranged_problems = ""
-    ops = {'-': (lambda op1, op2: int(op1) - int(op2)),
-           '+': (lambda op1, op2: int(op1) + int(op2))}
-    for problem in problems:
-        (op1, operator, op2) = [x for x in problem.split(" ")]
-        operation_func = ops[operator]
-        print(operation_func(op1, op2))
+import re
 
-        arranged_problems = arranged_problems + problem
-        print(problem)
-        return arranged_problems
+not_numeric = re.compile(r"[^\d]")
+ops = {'+': (lambda op1, op2: int(op1) + int(op2)),
+       '-': (lambda op1, op2: int(op1) - int(op2))}
+
+
+def arithmetic_arranger(problems, solve=False):
+    if len(problems) > 5:
+        return "Error: Too many problems."
+
+    arranged_problems = ""
+    row1, row2, row3, solution_row = [], [], ['----']*len(problems), []
+
+    allowed_ops = ops.keys()
+    for problem in problems:
+        # Error handling
+        (op1, operator, op2) = problem.split(" ")
+        if not_numeric.search(op1) or not_numeric.search(op2):
+            return "Error: Numbers must only contain digits."
+        if len(op1) > 4 or len(op2) > 4:
+            return "Error: Numbers cannot be more than four digits."
+        if operator not in allowed_ops:
+            return "Error: Operator must be '" + ("' or '".join(allowed_ops)) + "'."
+
+        row1.append(op1)
+        row2.append(operator + ' ' + op1)
+
+        # optional solve
+        if(solve):
+            operation_func = ops[operator]
+            solution_row.append(operation_func(op1, op2))
+
+    return arranged_problems
